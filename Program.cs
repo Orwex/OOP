04f6c74e9cb90bffd3352aaa25cc3e06f8b97d13
Orwex <44752155+Orwex@users.noreply.github.com>
2019-01-23 21:09:03 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _230119___Class_6
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Vehicles Mercedes = new Vehicles
+            {
+                year = 2020,
+                km = 0.0,
+                brand = "Mercedes",
+                model = "SAMG"
+            };
+            Console.WriteLine($"The name of the car {Mercedes.brand} and the model is{Mercedes.model}");
+            Console.WriteLine($"The KM of the car is {Mercedes.km} and the year is {Mercedes.year}");
+            Console.WriteLine();
+
+            Vehicles Koenigsegg = new Vehicles
+            {
+                year = 2019,
+                km = 1.1,
+                brand = "Koenigsegg",
+                model = "SXRR"
+
+            };
+            Console.WriteLine(Koenigsegg);
+            Console.WriteLine(Mercedes);
+
+
+
+
+        }
+
+    }
+}
+
---
+++ Vehicles.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _230119___Class_6
+{
+    class Vehicles
+    {
+        public int year;
+        public double km;
+        public string brand;
+        public string model;
+
+        public string Tellmeyourmodel()
+        {
+            return model;
+        }
+        public void drive()
+        {
+
+        }
+        public void start()
+        {
+
+        }
+        public double Tellmeyourkm()
+        {
+            return km;
+        }
+
+        public override string ToString()
+        {
+            return $"Brand = {brand} Model = {model} KM {km} Year = {year}";
+        }
+    }
+}
